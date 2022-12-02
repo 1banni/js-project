feac5572391eece5c122b25306e3c8b914df49e9
Will Bannister <bannister.will@gmail.com>
2022-12-01 17:21:08 -0800

    completed project folder initial setup

---
+++ dist/main.js
@@ -0,0 +1,60 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/src/index.js: Unexpected token (5:0)\n\n\u001b[0m \u001b[90m 3 |\u001b[39m \u001b[36mconst\u001b[39m canvas \u001b[33m=\u001b[39m document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'canvas'\u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 4 |\u001b[39m \u001b[36mconst\u001b[39m ctx \u001b[33m=\u001b[39m \u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 5 |\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n    at instantiate (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:67:32)\n    at constructor (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:364:12)\n    at Parser.raise (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:3365:19)\n    at Parser.unexpected (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:3398:16)\n    at Parser.parseExprAtom (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:11568:22)\n    at Parser.parseExprSubscripts (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:11171:23)\n    at Parser.parseUpdate (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:11153:21)\n    at Parser.parseMaybeUnary (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:11127:23)\n    at Parser.parseMaybeUnaryOrPrivate (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:10956:61)\n    at Parser.parseExprOps (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:10962:23)\n    at Parser.parseMaybeConditional (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:10937:23)\n    at Parser.parseMaybeAssign (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:10895:21)\n    at /Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:10863:39\n    at Parser.allowInAnd (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:12635:16)\n    at Parser.parseMaybeAssignAllowIn (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:10863:17)\n    at Parser.parseVar (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:13639:91)\n    at Parser.parseVarStatement (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:13458:10)\n    at Parser.parseStatementContent (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:13039:23)\n    at Parser.parseStatementLike (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:12952:17)\n    at Parser.parseModuleItem (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:12933:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:13558:36)\n    at Parser.parseBlockBody (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:13550:10)\n    at Parser.parseProgram (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:12842:10)\n    at Parser.parseTopLevel (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:12832:25)\n    at Parser.parse (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:14740:10)\n    at parse (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/parser/lib/index.js:14782:38)\n    at parser (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/core/lib/parser/index.js:41:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/core/lib/transformation/normalize-file.js:66:38)\n    at normalizeFile.next (<anonymous>)\n    at run (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/core/lib/transformation/index.js:21:50)\n    at run.next (<anonymous>)\n    at transform (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/@babel/core/lib/transform.js:22:41)\n    at transform.next (<anonymous>)\n    at step (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/gensync/index.js:261:32)\n    at /Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/gensync/index.js:273:13\n    at async.call.result.err.err (/Users/wbannister/Library/CloudStorage/OneDrive-Personal/aa/Projects/js-project/mario-battle-00/node_modules/gensync/index.js:223:11)");
+
+/***/ }),
+
+/***/ "./src/index.scss":
+/*!************************!*\
+  !*** ./src/index.scss ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguc2Nzcy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9tYXJpby1iYXR0bGUtMDAvLi9zcmMvaW5kZXguc2Nzcz85NzQ1Il0sInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.scss\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	__webpack_modules__["./src/index.js"](0, {}, __webpack_require__);
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.scss"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import _ from 'lodash';
+
+const canvas = document.getElementById('canvas');
+const ctx = 
