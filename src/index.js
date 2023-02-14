5f2c6c414141ab4e2f54b99e44c5cc05280f078f
1banni <bannister.will@gmail.com>
2023-02-13 19:46:08 -0800

    adding instructions

--- src/index.js
+++ src/index.js
@@ -2,33 +2,35 @@ import GameView from './scripts/game-view.js';
 import KeyHandler from './scripts/key-handler.js'
 import Game from "./scripts/game.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const startGameButton = document.getElementById('start-game');
-  startGameButton.addEventListener('click', startGame);
+// window.onload = function() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const startGameButton = document.getElementById('start-game');
+    startGameButton.addEventListener('click', startGame);
 
 
-  function startGame() {
-    const gameWindow = document.getElementById('game-window');
-    gameWindow.classList.remove("hidden");
-    const mainMenu = document.getElementById('main-menu');
-    mainMenu.classList.add("hidden");
-    // canvas.removeAttribute("style");
+    function startGame() {
+      const gameWindow = document.getElementById('game-window');
+      gameWindow.classList.remove("hidden");
+      const mainMenu = document.getElementById('main-menu');
+      mainMenu.classList.add("hidden");
+      // canvas.removeAttribute("style");
 
 
-    console.log('starting');
+      console.log('starting');
 
-    const canvas = document.getElementById('game-canvas');
-    const ctx = canvas.getContext("2d");
-    let CANVAS_WIDTH = canvas.width = 1200;
-    let CANVAS_HEIGHT = canvas.height = 800;
-    let gameView = new GameView(ctx);
-    // instantiate key handler and add event listeners for keyboard actions
+      const canvas = document.getElementById('game-canvas');
+      const ctx = canvas.getContext("2d");
+      let CANVAS_WIDTH = canvas.width = 1200;
+      let CANVAS_HEIGHT = canvas.height = 800;
+      let gameView = new GameView(ctx);
+      // instantiate key handler and add event listeners for keyboard actions
 
 
 
-      const menu = document.getElementById('menu');
-  }
+        const menu = document.getElementById('menu');
+    }
 
-  console.log('window', window);
+    console.log('window', window);
 
-});
+  });
+// }
